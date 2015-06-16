1da99c429e949af80d2b5e96ef83596f73596d42
infotainmentology <onlyifforanight@gmail.com>
2015-06-16 20:47:43 +0200

    initialization

--- Forest.c
+++ Forest.c
@@ -1,49 +1,72 @@
-
-#include <iostream>
-#include <cstdlib>
-//#include <cstdio>
-#include <ctime>
-#include <stdlib.h>
-#include <string.h>
-#include <string>
-#include <vector>
-#include <typeinfo>
 #include <mpi.h>
-#include <sstream>
-#include "./../include/Bunny.hpp"
-#include "./../include/Animal.hpp"
-#include "./../include/Teddy.hpp"
-#include "./../include/Meadow.hpp"
-#include "./../include/Constants.hpp"
-#include "./../include/PartyRequest.hpp"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <stddef.h>
+#include <pthread.h>
+#include <stdbool.h>
 
-using namespace std;
+#define maxMeadowCapacity 20
+
+#define TAG_JOIN 1
+#define TAG_ENTER 2
+#define TAG_LEAVE 3
+#define PARTY_NO 0
+#define PARTY_YES 1
+
+
+
+pthread_mutex_t dataMutex;
+pthread_mutex_t mpiMutex;
+int bunnyCount = 10;
+int teddyCount = 5;
+int meadowCount = 5;
+
+
+struct animal{
+   int id;
+   int meadowId;
+   int size;
+   int lamport;
+   int partying;
+};
+
+int * meadows;
+
+struct animal me;
+
+bool shouldIStartProgram()
+{
+	bool startProgram = true;
+
+	
+	return false;;
+}
+void initialize(int tid){
+
+
+
+}
 
 int main(int argc, char **argv)
 {
-    const string wrongInitializationMsg = " mpirun -np [number of processes] ./forest [number of bunnies] [number of teddies] [number of processes] \n \
-    Number of processes must equal number of bunnies + number of teddies";
-    Animal* me;
-    vector <Meadow*> meadows;
+
+
     int numOfThreads,tid;
+     bool startProgram = true;
 	
     MPI_Status status;
     MPI_Init(&argc, &argv); 
 
     MPI_Comm_size( MPI_COMM_WORLD, &numOfThreads );
     MPI_Comm_rank( MPI_COMM_WORLD, &tid );
+    
 
-    int bunnyCount = 10;
-    int teddyCount = 5;
-    int meadowCount = 5;
-    
-    bool startProgram = true;
-    
-    if (argc >=4)
+	if (argc >=4)
     { 
       	bunnyCount = atoi(argv[1]);
-	teddyCount = atoi(argv[2]);
-	meadowCount = atoi(argv[3]);
+		teddyCount = atoi(argv[2]);
+		meadowCount = atoi(argv[3]);
 	
 	if ( ((strcmp(argv[1], "0") != 0) && bunnyCount == 0) ||
 	    ((strcmp(argv[2], "0") != 0) && teddyCount == 0) ||
@@ -51,7 +74,7 @@ int main(int argc, char **argv)
 	{
 	    if (tid == 0)
 	    {
-		cerr << "Argv conversion unsuccessful" << endl;
+		printf("Argv conversion unsuccessful");
 		//MPI_Abort(MPI_COMM_WORLD, -1);
 	    }
 	    startProgram = false;
@@ -61,67 +84,50 @@ int main(int argc, char **argv)
     if ( (numOfThreads != bunnyCount + teddyCount) || (bunnyCount < 0) ||
 	(teddyCount < 0) || (meadowCount <1) )
     {
-	if (tid == 0)
-	{
-	    cerr << wrongInitializationMsg << endl;
-	    //MPI_Abort(MPI_COMM_WORLD, -1);
-	}
+		if (tid == 0)
+		{
+	    	printf("Argv conversion unsuccessful");
+		}
 	startProgram = false;
     }
-	
+    // startProgram = shouldIStartProgram();
+    
     //if no errors were made during initialization
     if (startProgram)
     {
+    	meadows = (int *) malloc(meadowCount * sizeof(int));
 	//if first bunny
 	if (tid == 0)
 	{
 		srand(time(NULL));
-
-		for (int i = 0; i < meadowCount; ++i)
+		int i;
+		for (i = 0; i < meadowCount; ++i)
 		{
-		  meadows.push_back(new Meadow(i, rand() % maxMeadowCapacity));
+		  meadows[i] = rand() % maxMeadowCapacity;
 	
 		}
-		
+		int thNum;
 		//broadcasting meadows
-		for (int thNum = 1; thNum < numOfThreads; thNum++)
+		for (thNum = 1; thNum < numOfThreads; thNum++)
 		{
-		  for (int i=0; i< meadowCount; i++)
-		  {
-		    
-		    int toSend[2];
-		    toSend[0] = meadows.at(i)->getId();
-		    toSend[1] = meadows.at(i)->getCapacity();
-		    MPI_Send(&toSend, 2, MPI_INT, thNum, 100, MPI_COMM_WORLD);
-		  }
+			MPI_Send(&meadows, meadowCount, MPI_INT, thNum, 100, MPI_COMM_WORLD);		  
 		}
 		  
 	  }
 	else //if not tid 0
 	{
-	    int toRecieve[2] = {0,0};
-	    for (int i=0; i< meadowCount; i++)
-	    {
-		MPI_Recv(&toRecieve, 2, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);	  
-		//cout << "tid = " <<  tid << ",recieved " << toRecieve[0] <<  "+" << toRecieve[1] << endl;
-		meadows.push_back(new Meadow(toRecieve[0], toRecieve[1]));
-	    }
+		MPI_Recv(&meadows, meadowCount, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);	  
 	}
 	
 	if (tid < bunnyCount)
 	{
-	    me = new Bunny(tid);
+	   ; 
 	}
 	else
 	{
-	    me = new Teddy(tid);
+	   ; 
 	}	
-	me->setMeadows(meadows);
-	me->setCounts(numOfThreads, bunnyCount, teddyCount, meadowCount);
-	me->party();
-	
-	//me -> broadcastPartyMsg();
-	//me -> handleRecievingMessages();
+
 	
     }
 
